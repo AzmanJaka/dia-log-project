382114f9b53c1ef7f03a96335c11536ab4a0186e
Jaka Azman <azman.jaka@gmail.com>
2016-03-16 13:43:02 +0000

    Full Width page, added bootflat, no new functionaity added.

---
+++ passport-mongo/public/javascript/script.js
@@ -0,0 +1,3 @@
+var img = '<img src="https://si0.twimg.com/a/1339639284/images/three_circles/twitter-bird-white-on-blue.png" />';
+
+$("#blob").popover({ title: 'Look!  A bird image!', content: img });
