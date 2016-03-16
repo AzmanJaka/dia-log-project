26eaf16173b1a5054488876d1ed6cf36b5cd0746
Jaka Azman <azman.jaka@gmail.com>
2016-03-16 21:12:20 +0000

    Testing Bootstrap panels and using them to display messages

--- passport-mongo/public/javascript/script.js
+++ passport-mongo/public/javascript/script.js
@@ -1,3 +1,5 @@
-var img = '<img src="https://si0.twimg.com/a/1339639284/images/three_circles/twitter-bird-white-on-blue.png" />';
-
-$("#blob").popover({ title: 'Look!  A bird image!', content: img });
+<script>
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
+</script>
